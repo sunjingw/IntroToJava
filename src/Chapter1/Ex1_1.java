a7633b400ab21424592e20bf325616df99d64128
sunjingw <sunjingw@sheridancollege.ca>
2023-09-13 14:00:06 -0400

    Chapter 1

---
+++ src/Chapter1/Ex1_1.java
@@ -0,0 +1,9 @@
+package Chapter1;
+
+public class Ex1_1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+    }
+}
---
+++ src/Chapter1/Ex1_2.java
@@ -0,0 +1,11 @@
+package Chapter1;
+
+public class Ex1_2 {
+    public static void main(String[] args){
+        System.out.println("Welome to Java");
+        System.out.println("Welome to Java");
+        System.out.println("Welome to Java");
+        System.out.println("Welome to Java");
+        System.out.println("Welome to Java");
+    }
+}
